a3919537b2b6820b284cfd37f5858928f5993b74
leah_ana <53849184+leahana@users.noreply.github.com>
2024-12-27 16:39:19 +0800

    暂时不使用回掉

--- BBM/MCH/MchRotationEventHandler.cs
+++ BBM/MCH/MchRotationEventHandler.cs
@@ -3,50 +3,6 @@ using AEAssist.CombatRoutine.Module;
 
 namespace BBM.MCH;
 
-public class MchRotationEventHandler:IRotationEventHandler
+public class MchRotationEventHandler
 {
-    public Task OnPreCombat()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnResetBattle()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task OnNoTarget()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSpellCastSuccess(Slot slot, Spell spell)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void AfterSpell(Slot slot, Spell spell)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnBattleUpdate(int currTimeInMs)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnEnterRotation()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnExitRotation()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnTerritoryChanged()
-    {
-        throw new NotImplementedException();
-    }
 }
