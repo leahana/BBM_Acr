634b32d461efc784331f2cb6cdf9cd8fb1e52eae
leah_ana <53849184+leahana@users.noreply.github.com>
2024-12-30 17:02:59 +0800

    新增战斗开始时间轴检测

--- BBM/MCH/Triggers/MchCondAfterBattleStart.cs
+++ BBM/MCH/Triggers/MchCondAfterBattleStart.cs
@@ -1,27 +1,27 @@
-
 using AEAssist.CombatRoutine.Trigger;
 using AEAssist.CombatRoutine.Trigger.Node;
 using AEAssist.GUI.Tree;
 
 namespace BBM.MCH.Triggers;
 
-public class MchCondAfterBattleStart:ITriggerCond,ITriggerlineCheck
+public class MchCondAfterBattleStart : ITriggerCond, ITriggerlineCheck
 {
     public bool Draw()
     {
-        throw new NotImplementedException();
+        return true;
     }
 
     public string DisplayName { get; }
     public string Remark { get; set; }
+
     public bool Handle(ITriggerCondParams condParams = null)
     {
-        throw new NotImplementedException();
+        return true;
     }
 
     public void Check(TreeCompBase parent, TreeNodeBase currNode, TriggerLine triggerLine, Env env,
         TriggerlineCheckResult checkResult)
     {
-        throw new NotImplementedException();
+        return;
     }
 }
